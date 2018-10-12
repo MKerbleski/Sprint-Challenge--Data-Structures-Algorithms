7f549787e21e10d5d1d09af431f9e42dc19e0fb5
mkerbleski <mkerbles@gmail.com>
2018-10-12 10:14:32 -0600

    complete 'depth_first_for_each' method

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,9 +5,48 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    #make function a lambda function
+
+    #have the callback iterate over eact item in depth first order utilizing stack data structure. 
+    
+    print('\nnew depth')
+    print(self.value, 'value')
+    if self.value != None:
+      cb(self.value)
+      print(self.value, 'appended')
+      if self.left != None:
+        print(self.left.value, 'self.left.value')
+        if self.left.left != None: 
+          print('self.left.left exists next line should be new depth')
+          self.left.depth_first_for_each(cb)
+        else: 
+          cb(self.left.value)
+          print(self.left.value, 'self.left.value appended')
+          if self.left.right != None:
+            print('something is self.left.right')
+            print('self.left.right.depth to be called')
+            self.left.right.depth_first_for_each(cb)
+      if self.right != None:
+        print(self.right.value, 'value-right')
+        self.right.depth_first_for_each(cb)
+        print(self.right.value, 'right')  
+      else: 
+        print('no left of right values')
+
+    # go left until you can go left no more 
+    #then go back a step and go right 
+    #repeat until everything is hit
+
 
   def breadth_first_for_each(self, cb):
+    #make function a lambda function
+
+    #same as above but with bredth first 
+
+    #go left first than right
+    #then repeat with children of the left 
+      #followed by children of the right 
+    #repeat until all done. 
     pass
 
   def insert(self, value):
